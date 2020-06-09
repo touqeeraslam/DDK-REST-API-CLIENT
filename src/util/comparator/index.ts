7934d0283264397596b0fd15ab8c6fa87cca2fb1
Touqeer Aslam <touqeer@mediapark.pk>
2020-06-09 11:44:34 +0500

    removed magic numbers and added const for ddk multipler value

--- src/util/comparator/index.ts
+++ src/util/comparator/index.ts
@@ -1,10 +1,11 @@
 export interface Comparator<T> {
     compare(a: T, b: T): number;
 }
+export const DDK_MULTIPLIER_CONSTANT = 100000000;
 
 export const convertMoney = (value: number) => {
     if (typeof value === 'number' && !isNaN(value) && value > 0) {
-        return value / 100000000;
+        return value / DDK_MULTIPLIER_CONSTANT;
     }
     return 0;
 };
